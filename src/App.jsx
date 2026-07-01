0e13b9095626462f6ddc972e6b0d2a970dca1543
YuriLucas7 <lucasy973@gmail.com>
2026-07-01 09:53:55 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 
